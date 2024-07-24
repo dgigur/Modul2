00ce75598023fe26d42844d8e3937d6e2dc1d74d
dgigur <dgigurda005@mail.ru>
2024-07-24 06:50:04 +0300

    Изменил вывод строки с [[],[]] на []

--- module2hard.py
+++ module2hard.py
@@ -5,15 +5,15 @@ password = []
 def uniq(list_1, a, b):
     in_list = False
     if len(list_1) == 0:
-        password.append([a, b])
+        password.extend([a, b])
         return
     else:
-        for k in range(0, len(list_1)):
-            if [a, b] == reversed(list_1[k]):
+        for k in range(0, len(list_1), 2):
+            if [a, b] == reversed([list_1[k], list_1[k+1]]):
                 in_list = True
                 break
     if not in_list:
-        password.append([a, b])
+        password.extend([a, b])
 
 
 for i in range(1, number):
@@ -23,15 +23,7 @@ for i in range(1, number):
         elif number % (i + j) == 0:
             uniq(password, i, j)
 
-print(password)
-
-
-#Это чтобы вывести ответ как в задании
-'''
-password1 = []
+print(*password)
 for i in range(len(password)):
-    password1.extend(password[i])
-for i in range(len(password1)):
-    password1[i] = str(password1[i])
-print(''.join(password1))
-'''
+    password[i] = str(password[i])
+print(''.join(password))
