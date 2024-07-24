95d3ad2d033e12af38a9b5c94a48e599a08bedea
dgigur <dgigurda005@mail.ru>
2024-07-24 20:11:23 +0300

    Укоротил

--- module2hard.py
+++ module2hard.py
@@ -4,16 +4,13 @@ password = []
 
 def uniq(list_1, a, b):
     in_list = False
-    if len(list_1) == 0:
-        password.extend([a, b])
-        return
-    else:
-        for k in range(0, len(list_1), 2):
-            if [a, b] == reversed([list_1[k], list_1[k+1]]):
-                in_list = True
-                break
-    if not in_list:
-        password.extend([a, b])
+    if len(list_1) == 0:  # Если список пустой то вернем что чисел нет в списке
+        return in_list
+    for k in range(0, len(list_1), 2):  # Прогоним проверку наличия пары чисел в списке
+        if [a, b] == reversed([list_1[k], list_1[k + 1]]):  # Идентичной пары быть не может поэтому только обратная
+            in_list = True
+            break
+    return in_list
 
 
 for i in range(1, number):
@@ -21,7 +18,8 @@ for i in range(1, number):
         if j == i:
             continue
         elif number % (i + j) == 0:
-            uniq(password, i, j)
+            if not uniq(password, i, j):  # Если флаг остался 0 то добавим пару чисел
+                password.extend([i, j])
 
 print(*password)
 for i in range(len(password)):
